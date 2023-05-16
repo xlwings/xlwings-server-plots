bd6eb7162ae90843a5204e9e0a32c55a29a3a150
Felix Zumstein <fzumstein@gmail.com>
2023-05-16 11:39:18 +0200

    use Book as context manager

--- app/api/timeseries.py
+++ app/api/timeseries.py
@@ -16,30 +16,30 @@ router = APIRouter(
 
 @router.post("/random-walk")
 async def random_walk(data: dict = Body):
-    book = xw.Book(json=data)
-    sheet = book.sheets[0]
+    with xw.Book(json=data) as book:
+        sheet = book.sheets[0]
 
-    # pandas DataFrame with a random walk
-    df = pd.DataFrame(
-        data=np.random.randn(500),
-        index=pd.date_range("1/1/2022", periods=500),
-        columns=["Time Series"],
-    )
-    df["Time Series"] = df["Time Series"].cumsum()
-    window = sheet["F1"].value
-    df[f"{window}d average"] = df["Time Series"].rolling(window).mean()
-    sheet["A1"].expand().clear_contents()
-    sheet["A1"].value = df
+        # pandas DataFrame with a random walk
+        df = pd.DataFrame(
+            data=np.random.randn(500),
+            index=pd.date_range("1/1/2022", periods=500),
+            columns=["Time Series"],
+        )
+        df["Time Series"] = df["Time Series"].cumsum()
+        window = sheet["F1"].value
+        df[f"{window}d average"] = df["Time Series"].rolling(window).mean()
+        sheet["A1"].expand().clear_contents()
+        sheet["A1"].value = df
 
-    # Matplotlib plot
-    plt.style.use("fivethirtyeight")
-    ax = df.plot(figsize=(12, 8))
-    sheet.pictures.add(
-        image=ax.get_figure(),
-        name="time_series",
-        anchor=sheet["E9"],
-        export_options={"bbox_inches": "tight", "dpi": 80},
-        update=True,
-    )
+        # Matplotlib plot
+        plt.style.use("fivethirtyeight")
+        ax = df.plot(figsize=(12, 8))
+        sheet.pictures.add(
+            image=ax.get_figure(),
+            name="time_series",
+            anchor=sheet["E9"],
+            export_options={"bbox_inches": "tight", "dpi": 80},
+            update=True,
+        )
 
-    return book.json()
+        return book.json()
